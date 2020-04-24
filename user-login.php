6dc1ba0136b36ad6d5b97c828f93b80f0f858e09
Jofrank Orozco <jofranko@uninorte.edu.co>
2020-04-24 11:23:06 -0500

    a

--- user-login.php
+++ user-login.php
@@ -2,11 +2,13 @@
 
 include('db.php');
 
-if(isset($_POST['user-log'])){
-    $username= $_POST['username'];
-    $password= $_POST['password'];
+session_start();
 
-    $query = "SELECT * FROM users WHERE username=$username AND password=$password";
+if(isset($_POST['user-log'])){
+    $username= '$_POST['username']';
+    $password= '$_POST['password']';
+
+    $query = "SELECT * FROM users WHERE username='$username' AND password='$password'";
     $result = mysqli_query($conn,$query);
     if(!$result){
         $_SESSION['logfail']="Unsuccessful entry";
