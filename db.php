0f39b2b9bcdf5226e7298e0a7b29927d40f169fb
Jofrank Orozco <jofranko@uninorte.edu.co>
2020-04-24 09:13:04 -0500

    s

--- db.php
+++ db.php
@@ -2,7 +2,7 @@
 
 $conn = mysqli_connect('b7ncxfxzwxonsblshjek-mysql.services.clever-cloud.com','u40wkasg5lvdfpad','3anD8tL91W2pJlMDVsTg','b7ncxfxzwxonsblshjek');
 
-if($conn == false){
+if($conn === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 } else{
 	session_start();
