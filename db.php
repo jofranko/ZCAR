c30b779a377bba4c5446de97f4b3e0e0b75da06c
Jofrank Orozco <jofranko@uninorte.edu.co>
2020-04-24 10:55:22 -0500

    a

--- db.php
+++ db.php
@@ -1,8 +1,17 @@
 <?php
 
-session_start();
+$db_host = "b7ncxfxzwxonsblshjek-mysql.services.clever-cloud.com";
+$db_user= "u40wkasg5lvdfpad";
+$db_password = "3anD8tL91W2pJlMDVsTg";
+$db_name= "b7ncxfxzwxonsblshjek";
 
-$conn = mysqli_connect('b7ncxfxzwxonsblshjek-mysql.services.clever-cloud.com','u40wkasg5lvdfpad','3anD8tL91W2pJlMDVsTg','b7ncxfxzwxonsblshjek');
+$conn = mysqli_connect($db_host,$db_user,$db_password,$db_name);
+
+if (!$conn) {
+	die("Connection failed: " . mysqli_connect_error());
+}
+
+session_start();
 
 ?>
 <!-- HOST, USER, PASSWORD, DB_NAME -->
