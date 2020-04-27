8a4ac78649e391609c6e90aff19aa39a6a48d1ac
Jofrank Orozco <jofranko@uninorte.edu.co>
2020-04-27 06:29:52 -0500

    fix

--- db.php
+++ db.php
@@ -4,10 +4,11 @@ define('DB_USER', 'u40wkasg5lvdfpad');
 define('DB_PASSWORD', '3anD8tL91W2pJlMDVsTg');
 define('DB_NAME', 'b7ncxfxzwxonsblshjek');
 
-session_start();
+
 $conn = mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME);
 
 if (!$conn) {
 	die("Connection failed: " . mysqli_connect_error());
+	session_start();
 }
 ?>
