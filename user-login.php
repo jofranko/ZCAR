98757c5f6687dd5ec991e22692f26e9d68b45ed2
Jofrank Orozco <jofranko@uninorte.edu.co>
2020-04-30 07:48:28 -0500

    test login 2

--- user-login.php
+++ user-login.php
@@ -8,12 +8,12 @@ if(isset($_POST['user_log'])){
 
     $query = "SELECT * FROM users WHERE users.username='$username' AND users.password='$password'";
     $result = mysqli_query($conn,$query);
-    if(!$result){
+    if(!isset($result)){
         $_SESSION['logfail']='Unsuccessful entry';
     }
-    if($result){
+    if(isset($result)){
         $_SESSION['logwell']='Successful entry';
-        $_SESSION['loguser']=$result;
+        $_SESSION['loguser']=$username;
     }
     header("Location: index.php");
 }
