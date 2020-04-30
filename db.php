4fbb9a09e65312d3d2179ea7e91e2fb860e71666
Jofrank Orozco <jofranko@uninorte.edu.co>
2020-04-30 08:37:44 -0500

    revert test db

--- db.php
+++ db.php
@@ -5,12 +5,12 @@ $DB_USER = "u40wkasg5lvdfpad";
 $DB_PASSWORD = "3anD8tL91W2pJlMDVsTg";
 $DB_NAME = "b7ncxfxzwxonsblshjek";
 
-$conn = new @mysqli_connect($DB_HOST,$DB_USER,$DB_PASSWORD,$DB_NAME);
+$conn = mysqli_connect($DB_HOST,$DB_USER,$DB_PASSWORD,$DB_NAME);
 
-if (!$conn) {
-	die("Connection failed: " . mysqli_connect_error());	
-} else{
+if ($conn) {
 	session_start();
+} else{
+	die("Connection failed: " . mysqli_connect_error());
 }
 
 ?>
