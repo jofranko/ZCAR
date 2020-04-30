be654e3a79521f5483aef92f30d53ba81aa071d1
Jofrank Orozco <jofranko@uninorte.edu.co>
2020-04-30 07:29:21 -0500

    a

--- index.php
+++ index.php
@@ -1,3 +1,26 @@
+<?php 
+
+if(!empty($_POST)){
+    require_once('db.php');
+    
+    $username= $_POST['l_username'];
+    $password= $_POST['l_password'];
+
+    $query = mysqli_query($conn,"SELECT * FROM users WHERE users.username='$username' AND users.password='$password'");
+    $result = mysqli_num_rows($query);
+    if($result>0){
+        session_start();
+        $_SESSION['logwell']=true;
+        $_SESSION['loguser']=$username;
+        header("Location: book.php");
+    }else{
+        $alert = "Wrong username or password"
+        header("Location: index.php");
+    }
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -229,25 +252,3 @@
   </body>
 </html>
 
-<?php 
-
-if(!empty($_POST)){
-    require_once('db.php');
-    
-    $username= $_POST['l_username'];
-    $password= $_POST['l_password'];
-
-    $query = mysqli_query($conn,"SELECT * FROM users WHERE users.username='$username' AND users.password='$password'");
-    $result = mysqli_num_rows($query);
-    if($result>0){
-        session_start();
-        $_SESSION['logwell']=true;
-        $_SESSION['loguser']=$username;
-        header("Location: book.php");
-    }else{
-        $alert = "Wrong username or password"
-        header("Location: index.php");
-    }
-}
-
-?>
