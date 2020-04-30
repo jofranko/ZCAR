28abd6462ed76b509be5fd1dd5c07ebeb1967e46
Jofrank Orozco <jofranko@uninorte.edu.co>
2020-04-30 08:33:55 -0500

    test db

--- db.php
+++ db.php
@@ -5,7 +5,7 @@ $DB_USER = "u40wkasg5lvdfpad";
 $DB_PASSWORD = "3anD8tL91W2pJlMDVsTg";
 $DB_NAME = "b7ncxfxzwxonsblshjek";
 
-$conn = mysqli_connect($DB_HOST,$DB_USER,$DB_PASSWORD,$DB_NAME);
+$conn = new @mysqli_connect($DB_HOST,$DB_USER,$DB_PASSWORD,$DB_NAME);
 
 if (!$conn) {
 	die("Connection failed: " . mysqli_connect_error());	
