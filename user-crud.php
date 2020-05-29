1b7f7f526337a2383f59abd8a81809b4a07d8045
Jofrank Orozco <jofranko@uninorte.edu.co>
2020-05-29 18:22:56 -0500

    fix crud 2

--- user-crud.php
+++ user-crud.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-<!-- DATABASE -->
-<?php include('db.php') ?>
+
 <head>
 	<meta charset="UTF-8">
 	<title>ZCAR</title>
@@ -34,102 +33,59 @@
         </div>
       </div>
     </nav>
-    <!-- crud -->
-    <div class="container">
-		<div class="content">
-			<h2>Lista de empleados</h2>
-			<hr />
+    <!-- CRUD -->
+    <div class="wrapper">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="page-header clearfix">
+                        <h2 class="pull-left">Users Details</h2>
+                    </div>
+                    <?php
+                    // Include config file
+                    require_once "db.php";
+                    
+                    // Attempt select query execution
+                    $sql = "SELECT * FROM users";
+                    if($result = mysqli_query($conn, $sql)){
+                        if(mysqli_num_rows($result) > 0){
+                            echo "<table class='table table-bordered table-striped'>";
+                                echo "<thead>";
+                                    echo "<tr>";
+                                        echo "<th>#</th>";
+                                        echo "<th>first name</th>";
+                                        echo "<th>last name</th>";
+                                        echo "<th>username</th>";
+                                        echo "<th>merbership</th>";
+                                    echo "</tr>";
+                                echo "</thead>";
+                                echo "<tbody>";
+                                while($row = mysqli_fetch_array($result)){
+                                    echo "<tr>";
+                                        echo "<td>" . $row['first_name'] . "</td>";
+                                        echo "<td>" . $row['last_name'] . "</td>";
+                                        echo "<td>" . $row['username'] . "</td>";
+                                        echo "<td>" . $row['merbership'] . "</td>";
+                                    echo "</tr>";
+                                }
+                                echo "</tbody>";                            
+                            echo "</table>";
+                            // Free result set
+                            mysqli_free_result($result);
+                        } else{
+                            echo "<p class='lead'><em>No records were found.</em></p>";
+                        }
+                    } else{
+                        echo "ERROR: Could not able to execute $sql. " . mysqli_error($conn);
+                    }
  
-			<?php
-			if(isset($_GET['aksi']) == 'delete'){
-				// escaping, additionally removing everything that could be (html/javascript-) code
-				$nik = mysqli_real_escape_string($con,(strip_tags($_GET["nik"],ENT_QUOTES)));
-				$cek = mysqli_query($con, "SELECT * FROM empleados WHERE codigo='$nik'");
-				if(mysqli_num_rows($cek) == 0){
-					echo '<div class="alert alert-info alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button> No se encontraron datos.</div>';
-				}else{
-					$delete = mysqli_query($con, "DELETE FROM empleados WHERE codigo='$nik'");
-					if($delete){
-						echo '<div class="alert alert-success alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button> Datos eliminado correctamente.</div>';
-					}else{
-						echo '<div class="alert alert-danger alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button> Error, no se pudo eliminar los datos.</div>';
-					}
-				}
-			}
-			?>
- 
-			<form class="form-inline" method="get">
-				<div class="form-group">
-					<select name="filter" class="form-control" onchange="form.submit()">
-						<option value="0">Filtros de datos de empleados</option>
-						<?php $filter = (isset($_GET['filter']) ? strtolower($_GET['filter']) : NULL);  ?>
-						<option value="1" <?php if($filter == 'Tetap'){ echo 'selected'; } ?>>Fijo</option>
-						<option value="2" <?php if($filter == 'Kontrak'){ echo 'selected'; } ?>>Contratado</option>
-                        <option value="3" <?php if($filter == 'Outsourcing'){ echo 'selected'; } ?>>Outsourcing</option>
-					</select>
-				</div>
-			</form>
-			<br />
-			<div class="table-responsive">
-			<table class="table table-striped table-hover">
-				<tr>
-                    <th>No</th>
-					<th>Código</th>
-					<th>Nombre</th>
-                    <th>Lugar de nacimiento</th>
-                    <th>Fecha de nacimiento</th>
-					<th>Teléfono</th>
-					<th>Cargo</th>
-					<th>Estado</th>
-                    <th>Acciones</th>
-				</tr>
-				<?php
-				if($filter){
-					$sql = mysqli_query($con, "SELECT * FROM empleados WHERE estado='$filter' ORDER BY codigo ASC");
-				}else{
-					$sql = mysqli_query($con, "SELECT * FROM empleados ORDER BY codigo ASC");
-				}
-				if(mysqli_num_rows($sql) == 0){
-					echo '<tr><td colspan="8">No hay datos.</td></tr>';
-				}else{
-					$no = 1;
-					while($row = mysqli_fetch_assoc($sql)){
-						echo '
-						<tr>
-							<td>'.$no.'</td>
-							<td>'.$row['codigo'].'</td>
-							<td><a href="profile.php?nik='.$row['codigo'].'"><span class="glyphicon glyphicon-user" aria-hidden="true"></span> '.$row['nombres'].'</a></td>
-                            <td>'.$row['lugar_nacimiento'].'</td>
-                            <td>'.$row['fecha_nacimiento'].'</td>
-							<td>'.$row['telefono'].'</td>
-                            <td>'.$row['puesto'].'</td>
-							<td>';
-							if($row['estado'] == '1'){
-								echo '<span class="label label-success">Fijo</span>';
-							}
-                            else if ($row['estado'] == '2' ){
-								echo '<span class="label label-info">Contratado</span>';
-							}
-                            else if ($row['estado'] == '3' ){
-								echo '<span class="label label-warning">Outsourcing</span>';
-							}
-						echo '
-							</td>
-							<td>
- 
-								<a href="edit.php?nik='.$row['codigo'].'" title="Editar datos" class="btn btn-primary btn-sm"><span class="glyphicon glyphicon-edit" aria-hidden="true"></span></a>
-								<a href="index.php?aksi=delete&nik='.$row['codigo'].'" title="Eliminar" onclick="return confirm(\'Esta seguro de borrar los datos '.$row['nombres'].'?\')" class="btn btn-danger btn-sm"><span class="glyphicon glyphicon-trash" aria-hidden="true"></span></a>
-							</td>
-						</tr>
-						';
-						$no++;
-					}
-				}
-				?>
-			</table>
-			</div>
-		</div>
-	</div><center>
+                    // Close connection
+                    mysqli_close($conn);
+                    ?>
+                </div>
+            </div>        
+        </div>
+    </div>
 
 	<!-- FOOTER -->
     <footer>
--- user-signin.php
+++ user-signin.php
@@ -15,7 +15,7 @@ if(isset($_POST['user_sign'])){
     $card_expiry_date= $_POST['s_card_expiry_date'];
     $card_cvv= $_POST['card_cvv'];
     
-    $query = "INSERT INTO users (first_name,last_name,email,username,password,identification,card_name,card_number,card_expiry_date,card_cvv) VALUES ('$first_name','$last_name','$email','$username','$password','$identification','$card_name','$card_number','$card_expiry_date','$card_cvv')";
+    $query = "INSERT INTO users (first_name,last_name,email,username,password,identification,membership,card_name,card_number,card_expiry_date,card_cvv) VALUES ('$first_name','$last_name','$email','$username','$password','$identification','$membership','$card_name','$card_number','$card_expiry_date','$card_cvv')";
     $result=mysqli_query($conn,$query);
     if (!$result){
         $repetido1= "SELECT * FROM users WHERE users.username=$username";
