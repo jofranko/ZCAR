1446215ab665f5b35213c81a9ea7cbee6f16c99f
Jofrank Orozco <jofranko@uninorte.edu.co>
2020-05-30 02:21:02 -0500

    f

--- contact-email.php
+++ contact-email.php
@@ -1,8 +1,8 @@
 <?php 
 
 if(isset($_POST['email'] && !empty($_POST['c_name']) && !empty($_POST['c_email']) && !empty($_POST['c_message'])) {
-    $to = 'jofranko@uninorte.edu.co';
-    $header = 'From:'.$_POST['c_email'];
+    $to = "jofranko@uninorte.edu.co";
+    $header = "From:".$_POST['c_email'];
     $subject = $_POST['c_name'];
     $message = $_POST['c_message'];
     mail($to,$subject,$message,$header);
