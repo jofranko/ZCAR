11dd27f5f87e7339d35504574d890c179d45eb7a
Jofrank Orozco <jofranko@uninorte.edu.co>
2020-05-30 05:09:06 -0500

    crud update fix

--- crud-element-delete.php
+++ crud-element-delete.php
@@ -8,9 +8,11 @@ if(isset($_POST['c_d']) && !empty($_POST['c_d_id'])){
     if ($table == 'book') {
     	$query = "DELETE FROM book WHERE id = '$id'";
     	$result = mysqli_query($conn,$query);
+    	header("Location: crud.php");
     }elseif ($table == 'users') {
     	$query = "DELETE FROM users WHERE id = '$id'";
     	$result = mysqli_query($conn,$query);
+    	header("Location: crud.php");
     }else{
     	header("Location: control-error.php");
     }
